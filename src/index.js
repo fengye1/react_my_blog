59191262ca4c1d5a832d6f307212da234c7e925d
lvpengfei <lvpengfei@now-do.com>
2018-04-16 17:04:29 +0800

    引入redux， react-redux, material-ui

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducer from './reducers/index'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <MuiThemeProvider>
+            <App />
+        </MuiThemeProvider>
+    </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
