278df80dc70abf507050438863d6b91ac286950c
lvpengfei <lvpengfei@now-do.com>
2018-04-21 15:46:03 +0800

    调整首页

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,12 +1,40 @@
 import React, {Component} from 'react';
 import HomeTop from './home_top'
-
-
+import {TextField, FontIcon} from 'material-ui'
+import './home.css';
 class Home extends Component{
     render(){
         return(
-            <HomeTop/>
+            
+            <div  className="main_home">
+                <HomeTop />
+                <TextField
+                    disabled={true}
+                    underlineShow={false}
+                    value={"个人小栈"}
+                    inputStyle={s_home.host_title}
+                />
+                 <FontIcon className="material-icons" style={s_home.icon}>home</FontIcon>
+                 
+            </div>
         )
     }
 }
+const s_home = {
+    main:{
+        height: 400
+    },
+    host_title:{
+        backgroundColor: 'blue',
+        color: '#fff',
+        fontSize: 40,
+        padding:10
+    },
+    icon:{
+        fontSize:24,
+        color:'red'
+    }
+}
+
 export default Home
+
